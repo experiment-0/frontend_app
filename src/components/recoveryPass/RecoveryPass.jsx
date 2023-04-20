750e6600dffd67ed7672da0fded9fa9677467dae
Vitaliy <vital10@inbox.ru>
2023-04-20 18:52:44 +0300

    recovery page

--- src/components/recoveryPass/RecoveryPass.jsx
+++ src/components/recoveryPass/RecoveryPass.jsx
@@ -1,6 +1,34 @@
+import { Link } from "react-router-dom"
+import style from "./RecoveryPass.module.css"
+
 const RecoveryPass = () => {
   return (
-    <div>RecoveryPass</div>
+    <div className={style.container}>
+      <div className={style.logo_block}>
+        <div className={style.platform_name}>Название платформы</div>
+        <Link to="/" className={style.registration}>
+          Назад
+        </Link>
+      </div>
+      <div className={style.form}>
+        <h3 className={style.title}>Забыли пароль?</h3>
+        <p className={style.recovery_text}>
+          Введите адрес почты, с которым вы регистрировались, и мы отправим
+          ссылку для сброса пароля.
+        </p>
+        <form className={style.authorisation_form}>
+          <input
+            className={style.form_input}
+            type="email"
+            placeholder="Email"
+          />
+
+          <button className={style.button} type="submit">
+            Отправить
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
